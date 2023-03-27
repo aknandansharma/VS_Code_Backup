cbe2ce58d60ee678efff9489ecfb6c42010caf4e
aknandansharma <aknandansharma@gmail.com>
2023-03-27 14:03:05 +0530

    basic is complited

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,7 +1,40 @@
-const EventEmitter = require("events");
-class MyEmitter extends EventEmitter {}
-const myEmitter = new MyEmitter();
-myEmitter.on("event", () => {
-  console.log("an event occurred!");
-});
-myEmitter.emit("event");
+// import {sum, diff} from './lib.js'
+// console.log(sum(4,5), diff(9,4));
+
+const fs = require('fs')
+
+
+// Create file Using Async
+// fs.writeFile("Text.txt", "This is write file", (err, data) => {
+//   if(err)
+//     console.log(err);
+//   console.log('file is created' + data);
+// })
+
+// // update file Using Async
+// fs.appendFile("Text.txt", " Created by Aknanddan sharma using update methods", (err, data) => {
+//   if(err)
+//     console.log(err);
+//   console.log(`file is updated ${data}`);
+// })
+
+// // read file Using Async
+// fs.readFile("Text.txt",  (err, data) => {
+//   if(err)
+//     console.log(err);
+//   console.log(`file is read\n ${data}`);
+// })
+// rename file Using Async
+// fs.rename("test.js", "test1.html",  (err, data) => {
+//   if(err)
+//     console.log(err);
+//   console.log(`file is read\n ${data}`);
+// })
+// rename file Using Async
+// fs.unlink("test1.html", (err, data) => {
+//   if(err)
+//     console.log(err);
+//   console.log(`file is read\n ${data}`);
+// })
+
+console.log("Aknandan sharma")
