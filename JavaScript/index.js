0274b5c5936e7f7ae3f9a5643ad08bd68d6777ce
aknandansharma <aknandansharma@gmail.com>
2023-03-28 21:20:41 +0530

    Servers

---
+++ JavaScript/index.js
@@ -0,0 +1,31 @@
+const http = require('http')
+const fs = require('fs')
+
+const index = fs.readFileSync('index.html')
+const data = JSON.parse(fs.readFileSync('data.json'))
+const product = data.products[1];
+
+const server = http.createServer((req, res) => {
+    console.log("Server start")
+
+    const url = req.url
+
+    if(url === '/'){
+        console.log('Home server');
+        res.end("<h1>This is Home Page</h1>")
+    }
+    else if(url === '/json'){
+        console.log('JSON server');
+        res.end(JSON.stringify(data))
+    }else if(url === '/html'){
+        console.log('HTML server');
+        res.end(index)
+    }else if(url === '/pro'){
+      res.end(data);
+    }else{
+        console.log('404 Error');
+        res.write("<h1>404 Page is Not Found</h1>");
+        res.end()
+    }
+})
+server.listen(8080)
