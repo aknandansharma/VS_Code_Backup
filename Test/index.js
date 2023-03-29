8310ef0e3cb94a6aed9ee9239e9b585381d0e2c8
aknandansharma <aknandansharma@gmail.com>
2023-03-29 10:10:37 +0530

    this JS Test

---
+++ Test/index.js
@@ -0,0 +1,41 @@
+/* Values and Variables */
+
+// var myName = "aknandan"
+// var _my__Name = "ak"
+// var $ak = "ak"
+// console.log(_my__Name);
+
+
+/* Data Types in JavaScript */
+// var myName = "🤪";
+// var myName = true
+// console.log(typeof myName);
+
+// console.log(false - true);
+
+// var test = null
+// console.log(test);
+// console.log(typeof test);
+
+// var test1
+// console.log(typeof test1);
+
+// var myPhoneNumber = 9876543210
+// var myName = "Aknandan"
+
+// console.log(isNaN(myPhoneNumber));
+// console.log(isNaN(myName));
+
+// console.log(NaN === NaN);
+// console.log(Number.NaN === Number.NaN);
+// console.log(isNaN(NaN));
+// console.log(isNaN(Number.NaN));
+// console.log(Number.isNaN(Number.NaN));
+ 
+
+// var x = 5
+// var y = 5
+// console.log(`This is x equal to y  ${x == y}`);
+// console.log(`This is x equal to y  ${x == y}`);
+
+// start with 1:20
