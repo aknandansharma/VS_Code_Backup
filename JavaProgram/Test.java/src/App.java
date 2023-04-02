2628d10ec17f786d1e8b3c6868ebe1a40a274327
aknandansharma <aknandansharma@gmail.com>
2023-04-02 10:59:30 +0530

    java updated

--- JavaProgram/Test.java/src/App.java
+++ JavaProgram/Test.java/src/App.java
@@ -2,11 +2,26 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
-         System.out.println("Enter String");
-         String s = sc.nextLine();
-         int e = s.length();
-         char ab = "\00000";
+      Scanner sc = new Scanner(System.in);
+      int x[] = new int[10];
+      System.out.println("Enter 10 numbers: ");
+      for (int i = 0; i < 10; i++) {
+        x[i] = sc.nextInt();
+      }
+
+      int gr = x[0], sm = x[0];
+      for (int i = 0; i < 10; i++) {
+        if (x[i] > gr) {
+          gr = x[i];
+        }
+        if (x[i] < sm) {
+          sm = x[i];
+        }
+      }
+      System.out.println(gr);
+      System.out.println(sm);
             
     }
 }
+
+//  
