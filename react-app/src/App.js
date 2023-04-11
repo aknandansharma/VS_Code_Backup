20480f4f01e666f70736ffc1d0496080ea7bcd1f
Aknandan Kumar <aknandansharma@gmail.com>
2023-04-11 13:54:54 +0530

    react useState complited

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -9,14 +9,14 @@ import AddIcon from "@mui/icons-material/Add";
 import DeleteIcon from "@mui/icons-material/Delete";
 
 function App() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
+  // const [name, setName] = useState("");
+  // const [email, setEmail] = useState("");
+  const [form, setForm] = useState({})
   const [data, setData] = useState([]);
 
   const addData = () => {
-    setData([...data, { name, email }]);
-    setName("");
-    setEmail("");
+    setData([...data, form]);
+    setForm({form})
   };
 
   const removeItem = (index) => {
@@ -33,15 +33,15 @@ function App() {
       <div className="main">
         <Stack direction="row" spacing={2}>
           <TextField
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            value={form.name}
+            onChange={(e) => setForm({...form, name: e.target.value})}
             id="outlined-basic"
             label="Name"
             variant="outlined"
           />
           <TextField
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            value={form.email}
+            onChange={(e) => setForm({...form, email: e.target.value})}
             id="outlined-basic"
             label="Email"
             variant="outlined"
