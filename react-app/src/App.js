d7b63274ce2edf0dcb7a4439385a85c7dcba7ac9
Aknandan Kumar <aknandansharma@gmail.com>
2023-04-13 22:53:36 +0530

    react useEffect complited

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,43 @@
-import React, {useState, useEffect} from 'react'
-import './App.css'
-import Header from './components/Header'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./components/Header";
 
 const App = () => {
-  const [state, setState] = useState(0 )
+  const [state, setState] = useState(2);
+  const [data, setData] = useState([]) 
+
+  async function getData() {
+    const get = await fetch(
+      `https://hub.dummyapis.com/employee?noofRecords=${state}&idStarts=1001`
+    );
+     const res = await get.json()
+     setData(res) 
+     document.title = (`${state} emp`) // show live count the data
+     console.log(res); 
+  }
 
   useEffect(() => {
-     window.alert ("Use Effect Called ");
-  })
+    getData()
+    
+  }, [state]); 
 
-  console.log('Function Body');
 
   return (
-    <div className='App'>
-      <Header/> 
-      <button onClick={() => setState(state 
-        +1)}>Click Me {state} </button>
+    <div className="App">
+      <Header />
+      <button onClick={() => setState(state + 3)}>Click Me {state} </button>
+      {data.map((e, index) => {
+        return (
+          <div key={index}>
+          <h4>{e.firstName}</h4>
+          <h4>{e.lastName}</h4>
+          <h4>{e.email}</h4>
+
+        </div>
+        )
+      })}
     </div>
-  )
-}
+  );
+};
 // start the use effect
-export default App
+export default App;
